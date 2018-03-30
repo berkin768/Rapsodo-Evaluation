17a6663cf3ebc36a9e127bbe76e58c0722a028ee
Berkin Akaydın <berkinnakaydinn@gmail.com>
2018-03-30 15:59:37 +0300

    Create Table And Print Values

--- main.js
+++ main.js
@@ -15,9 +15,69 @@ function checkFileAPI() {
     }
 }
 
-function getNumberOfPlayers(header){
-    header = header.filter(value => Object.keys(value).length !== 0);  //TO PREVENT STRANGE ',',',',',' ERROR
+function getNumberOfPlayers(header) {
+    header = header.filter(value => Object.keys(value).length !== 0); //TO PREVENT STRANGE ',',',',',' ERROR
     return header.length / 2
+}
+
+function drawPlayer(array) {
+    for (var i = 0; i < array.length; i++) {
+        var table = document.createElement("table");
+        table.className = 'table'
+
+        var header = table.createTHead()
+        table.setAttribute("id", "player" + (i+1));
+        document.body.appendChild(table);
+
+        var row = header.insertRow(0)
+
+        Object.keys(array[i][1]).forEach(function(v) {
+            var cell = document.createElement("th");
+            row.appendChild(cell);
+            var text = ''
+
+            switch(v){
+                case 'position':
+                text = 'Position'
+                break;
+                case 'total_shots':
+                text = 'Number of Shots'
+                break;
+                case 'successful_shots':
+                text = 'Successful Shots'
+                break;
+                case 'success_rate':
+                text = 'Success Rate'
+                break;
+            }
+            cell.innerHTML = text;
+          });
+        var tbody = table.createTBody()
+        
+        for (var j = 1; j < array[i].length; j++) {
+            var row = document.createElement("tr");
+            tbody.appendChild(row)
+
+            var position = array[i][j].position
+            var total_shots =  array[i][j].total_shots
+            var successful_shots = array[i][j].successful_shots
+            var success_rate = array[i][j].success_rate
+
+            var position_cell = document.createElement("td");
+            var total_shots_cell = document.createElement("td");
+            var successful_shots_cell = document.createElement("td");
+            var success_rate_cell = document.createElement("td");
+            position_cell.innerHTML= position
+            total_shots_cell.innerHTML = total_shots
+            successful_shots_cell.innerHTML= successful_shots
+            success_rate_cell.innerHTML = '%' + success_rate.toFixed(2)
+
+            row.appendChild(position_cell);
+            row.appendChild(total_shots_cell);
+            row.appendChild(successful_shots_cell);
+            row.appendChild(success_rate_cell);
+        }
+    }
 }
 
 /**
@@ -31,32 +91,45 @@ function readText(filePath) {
             var allTextLines = output.split(/\r\n|\r|\n/);
 
             var numberOfPlayers = getNumberOfPlayers(allTextLines[1].split(','))
-            var playerArray = new Array (numberOfPlayers)
+            var playerArray = new Array(numberOfPlayers)
 
-            for(var i=3; i < allTextLines.length; i++){
+            for (var i = 3; i < allTextLines.length; i++) {
                 var line = allTextLines[i].split(',').filter(value => Object.keys(value).length !== 0);
                 var indice = 0
-               
-                for(var j=0; j < line.length; j += 2){
+
+                for (var j = 0; j < line.length; j += 2) {
                     var position = line[j]
                     var positionValue = parseInt(line[j + 1])
 
-                    if(typeof playerArray[indice] === 'undefined'){
+                    //Initialize 
+                    if (typeof playerArray[indice] === 'undefined') {
                         playerArray[indice] = []
                     }
-                    
-                    if(typeof playerArray[indice][position] === 'undefined'){
-                        playerArray[indice][position] = {region : 0,total_shots : 0, successful_shots : 0}
+
+                    if (typeof playerArray[indice][position] === 'undefined') {
+                        playerArray[indice][position] = {
+                            region: 0,
+                            total_shots: 0,
+                            successful_shots: 0
+                        }
                     }
+
                     var total_shots = playerArray[indice][position].total_shots
                     var successful_shots = playerArray[indice][position].successful_shots
-                    playerArray[indice][position]  = {region : position, total_shots : total_shots + 1, successful_shots : successful_shots + positionValue}
+                    var rate = successful_shots / total_shots * 100
+
+                    playerArray[indice][position] = {
+                        position: position,
+                        total_shots: total_shots + 1,
+                        successful_shots: successful_shots + positionValue,
+                        success_rate : rate
+                    }
 
                     indice++
                 }
             }
-            
-            console.log(playerArray)
+            drawPlayer(playerArray)
+            //console.log(playerArray)
             //displayContents(output);
         }; //end onload()
         reader.readAsText(filePath.files[0]);
