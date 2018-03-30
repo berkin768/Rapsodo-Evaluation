4b73857d71e62453f86d58ab1f5e6b6c9f2d4837
Berkin Akaydın <berkinnakaydinn@gmail.com>
2018-03-30 18:40:16 +0300

    Fixed An Error While Calculating Rate

--- js/main.js
+++ js/main.js
@@ -19,61 +19,55 @@ function getNumberOfPlayers(header) {
 }
 
 function drawPlayer(array) {
+    console.log(array)
     for (var i = 0; i < array.length; i++) {
+
+        var tag = document.createElement('h4')
+        tag.innerHTML = 'Player ' + (i + 1)
+        tag.setAttribute('style', 'text-align:center')
+        document.body.appendChild(tag);
+
         var table = document.createElement("table");
         table.className = 'table'
+        table.setAttribute("id", "player" + (i + 1));
 
         var header = table.createTHead()
-        table.setAttribute("id", "player" + (i+1));
         document.body.appendChild(table);
 
-        var row = header.insertRow(0)
 
-        Object.keys(array[i][1]).forEach(function(v) {
+        var row = header.insertRow(0)
+        Object.keys(array[i][1]).forEach(function (v) {
             var cell = document.createElement("th");
             row.appendChild(cell);
             var text = ''
 
-            switch(v){
+            switch (v) {
                 case 'position':
-                text = 'Position'
-                break;
+                    text = 'Position'
+                    break;
                 case 'total_shots':
-                text = 'Number of Shots'
-                break;
+                    text = 'Number of Shots'
+                    break;
                 case 'successful_shots':
-                text = 'Successful Shots'
-                break;
+                    text = 'Successful Shots'
+                    break;
                 case 'success_rate':
-                text = 'Success Rate'
-                break;
+                    text = 'Success Rate'
+                    break;
             }
             cell.innerHTML = text;
-          });
+        });
         var tbody = table.createTBody()
-        
+
         for (var j = 1; j < array[i].length; j++) {
             var row = document.createElement("tr");
             tbody.appendChild(row)
 
-            var position = array[i][j].position
-            var total_shots =  array[i][j].total_shots
-            var successful_shots = array[i][j].successful_shots
-            var success_rate = array[i][j].success_rate
-
-            var position_cell = document.createElement("td");
-            var total_shots_cell = document.createElement("td");
-            var successful_shots_cell = document.createElement("td");
-            var success_rate_cell = document.createElement("td");
-            position_cell.innerHTML= position
-            total_shots_cell.innerHTML = total_shots
-            successful_shots_cell.innerHTML= successful_shots
-            success_rate_cell.innerHTML = '%' + success_rate.toFixed(2)
-
-            row.appendChild(position_cell);
-            row.appendChild(total_shots_cell);
-            row.appendChild(successful_shots_cell);
-            row.appendChild(success_rate_cell);
+            Object.values(array[i][j]).forEach(function (value) {
+                var cell = document.createElement("td")
+                cell.innerHTML = (value === +value && value !== (value | 0)) ? '%' + value.toFixed(2) : value //to recognize float and make it 2 number after ,
+                row.appendChild(cell)
+            });
         }
     }
 }
@@ -82,53 +76,52 @@ function drawPlayer(array) {
  * read text input
  */
 function readText(filePath) {
-    var output = ""; //placeholder for text output
     if (filePath.files && filePath.files[0]) {
         reader.onload = function (e) {
-            output = e.target.result;
+            var output = e.target.result;
             var allTextLines = output.split(/\r\n|\r|\n/);
 
             var numberOfPlayers = getNumberOfPlayers(allTextLines[1].split(','))
             var playerArray = new Array(numberOfPlayers)
 
+            //first 3 lines are empty or header, thats why this loop starts from 3
             for (var i = 3; i < allTextLines.length; i++) {
-                var line = allTextLines[i].split(',').filter(value => Object.keys(value).length !== 0);
-                var indice = 0
+                var line = allTextLines[i].split(',').filter(value => Object.keys(value).length !== 0); //filter operation to remove empty commas from the line. Ex 1,1,1,,,,,,,,,
+                var index = 0
 
                 for (var j = 0; j < line.length; j += 2) {
                     var position = line[j]
                     var positionValue = parseInt(line[j + 1])
 
-                    //Initialize 
-                    if (typeof playerArray[indice] === 'undefined') {
-                        playerArray[indice] = []
+                    //Initialize player
+                    if (typeof playerArray[index] === 'undefined') {
+                        playerArray[index] = []
                     }
 
-                    if (typeof playerArray[indice][position] === 'undefined') {
-                        playerArray[indice][position] = {
+                    //Initialize shot
+                    if (typeof playerArray[index][position] === 'undefined') {
+                        playerArray[index][position] = {
                             region: 0,
                             total_shots: 0,
                             successful_shots: 0
                         }
                     }
 
-                    var total_shots = playerArray[indice][position].total_shots
-                    var successful_shots = playerArray[indice][position].successful_shots
+                    var total_shots = playerArray[index][position].total_shots + 1
+                    var successful_shots = playerArray[index][position].successful_shots + positionValue
                     var rate = successful_shots / total_shots * 100
 
-                    playerArray[indice][position] = {
+                    playerArray[index][position] = {
                         position: position,
-                        total_shots: total_shots + 1,
-                        successful_shots: successful_shots + positionValue,
-                        success_rate : rate
+                        total_shots: total_shots,
+                        successful_shots: successful_shots,
+                        success_rate: rate
                     }
 
-                    indice++
+                    index++
                 }
             }
             drawPlayer(playerArray)
-            //console.log(playerArray)
-            //displayContents(output);
         }; //end onload()
         reader.readAsText(filePath.files[0]);
     } //end if html5 filelist support
@@ -152,15 +145,37 @@ function readText(filePath) {
     return true;
 }
 
-function onClickPDF() {
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    pdf.canvas.height = 72 * 11;
-    pdf.canvas.width = 72 * 8.5;
+document.addEventListener('DOMContentLoaded', function () {
+    function onClickPDF() {
+        var pdf = new jsPDF('p', 'pt', 'a4');
+        pdf.canvas.height = 72 * 11;
+        pdf.canvas.width = 72 * 8.5;
 
-    pdf.fromHTML(document.body);
+        pdf.fromHTML(document.body);
 
-    pdf.save('output.pdf');
-};
+        pdf.save('output.pdf');
+    };
 
-var element = document.getElementById("clickbind");
-element.addEventListener("click", onClickPDF);
+    /*function onClickPDF() {
+        var doc = new jsPDF();
+        var elementHandler = {
+            '#ignorePDF': function (element, renderer) {
+                return true;
+            }
+        };
+        var source = window.document.getElementsByTagName("body")[0];
+        doc.fromHTML(
+            source,
+            15,
+            15, {
+                'width': 180,
+                'elementHandlers': elementHandler
+            });
+
+        doc.output("dataurlnewwindow");
+    }*/
+
+
+    var element = document.getElementById("clickbind");
+    element.addEventListener("click", onClickPDF)
+});
